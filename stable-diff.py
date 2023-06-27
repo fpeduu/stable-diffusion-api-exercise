c72f19b9577f474e52a617325018e131f753fca6
lipe <fefs@cin.ufpe.br>
2023-06-27 15:45:58 -0300

    exercise

---
+++ stable-diff.py
@@ -0,0 +1,29 @@
+import os
+import requests
+import webbrowser
+
+API_KEY = 'sua chave de API'
+URL = 'https://stablediffusionapi.com/api/v3/text2img'
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY') # Sua chave de API
+URL = 'https://stablediffusionapi.com/api/v3/text2img'
+
+prompt = str(input("Enter your prompt: "))
+
+data = {
+      'prompt': prompt,
+      'key': API_KEY,
+      'width': 1024,
+      'height': 1024
+    }
+
+response = requests.post(URL, data=data).json()
+
+output = response['output']
+output = output[0]
+
+webbrowser.open(output)
